3304ac03ba838ab14485afddfddd2648bcc5771e
Ranxin2023 <rstli@ucdavis.edu>
2022-11-02 09:45:23 -0700

    phase3changed

--- tests/sem.c
+++ tests/sem.c
@@ -1,6 +1,6 @@
 #include <stddef.h>
 #include <stdlib.h>
-#include <pthread.h>
+
 #include "queue.h"
 #include "sem.h"
 #include "private.h"
@@ -8,8 +8,6 @@
 struct semaphore {
 	/* TODO Phase 3 */
 	int state;
-	int lock;
-	queue_t q;
 };
 
 sem_t sem_create(size_t count)
@@ -23,25 +21,17 @@ sem_t sem_create(size_t count)
 int sem_destroy(sem_t sem)
 {
 	/* TODO Phase 3 */
-	free(sem);
 }
 
 int sem_down(sem_t sem)
 {
 	/* TODO Phase 3 */
-	spinlock_lock(sem->lock);
-	while(sem->state==0){
-		/*Block self*/
-	}
 	sem->state-=1;
-	spinlock_unlock(sem->lock);
 }
 
 int sem_up(sem_t sem)
 {
 	/* TODO Phase 3 */
-	spinlock_lock(sem->lock);
 	sem->state+=1;
-	spinlock_unlock(sem->lock);
 }
 
