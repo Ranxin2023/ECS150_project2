dfc37c59655a4199212bdc2a0db1337da85bd85c
Ranxin2023 <rstli@ucdavis.edu>
2022-11-03 02:35:41 -0700

    renew uthread.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -5,6 +5,17 @@
 #include "sem.h"
 #include "private.h"
 
+
+/*
+ * sem_t - Semaphore type
+ *
+ * A semaphore is a way to control access to a common resource by multiple
+ * threads. Such resource has an internal count, meaning that it can only be
+ * shared a certain number of times. When a thread successfully takes the
+ * resource, the count is decreased. When the resource is not available,
+ * following threads are blocked until the resource becomes available again.
+ */
+
 struct semaphore {
 	/* TODO Phase 3 */
 	int state;
@@ -12,6 +23,16 @@ struct semaphore {
 	queue_t q;
 };
 
+
+/*
+ * sem_create - Create semaphore
+ * @count: Semaphore count
+ *
+ * Allocate and initialize a semaphore of internal count @count.
+ *
+ * Return: Pointer to initialized semaphore. NULL in case of failure when
+ * allocating the new semaphore.
+ */
 sem_t sem_create(size_t count)
 {
 	/* TODO Phase 3 */
@@ -29,19 +50,33 @@ int sem_destroy(sem_t sem)
 int sem_down(sem_t sem)
 {
 	/* TODO Phase 3 */
-	spinlock_lock(sem->lock);
+	spinlock_lock(sem);
 	while(sem->state==0){
 		/*Block self*/
+		
 	}
 	sem->state-=1;
-	spinlock_unlock(sem->lock);
+	spinlock_unlock(sem);
 }
+
+/*
+ * sem_up - Release a semaphore
+ * @sem: Semaphore to release
+ *
+ * Release a resource to semaphore @sem.
+ *
+ * If the waiting list associated to @sem is not empty, releasing a resource
+ * also causes the first thread (i.e. the oldest) in the waiting list to be
+ * unblocked.
+ *
+ * Return: -1 if @sem is NULL. 0 if semaphore was successfully released.
+ */
 
 int sem_up(sem_t sem)
 {
 	/* TODO Phase 3 */
-	spinlock_lock(sem->lock);
+	spinlock_lock(sem);
 	sem->state+=1;
-	spinlock_unlock(sem->lock);
+	spinlock_unlock(sem);
 }
 
